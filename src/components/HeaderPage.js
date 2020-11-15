bf46f089959c6d91836fe9eed2f0283dfd7eee02
Nenad Sparavalo <nenadsparavalo@Nenads-MacBook-Pro.local>
2020-11-15 10:44:32 +0100

    Header height update

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -5,8 +5,8 @@ export default function HeaderPage() {
 	return (
 		<div className="header-layout vh-100 ">
 			<div className="layout"></div>
-			<div className="container">
-				<div className="row d-flex vh-100 justify-content-center align-items-center">
+			<div className="container vh-100 d-flex justify-content-center align-items-center">
+				<div className="row">
 					<div className="col-12">
 						<h1 className="my-2 display-1 text-center font-weight-bolder">
 							Online Shop
